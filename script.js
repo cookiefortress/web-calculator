640e8cf5790ff2e93bd7d6264a6a4d03185f975e
cookiefortress <iwantcookieplz@gmail.com>
2025-03-21 11:23:18 -0400

    Add initial version of the calculator's script

--- script.js
+++ script.js
@@ -1,36 +1,114 @@
 console.log("Script loaded");
 
-let numOne = 0;
-let numTwo = 0;
 let operator = "";
+let numContainer = "";
+let num = "";
+
 
 let display = document.querySelector("#display");
 
 let one = document.querySelector("#one");
 one.addEventListener("click", () => {
-    if (display.textContent.includes(operator)) numTwo += 1;
-    else numOne += 1;
+    num += "1";
+    display.textContent = num;
+});
+let two = document.querySelector("#two");
+two.addEventListener("click", () => {
+    num += "2";
+    display.textContent = num;
+});
+let three = document.querySelector("#three");
+three.addEventListener("click", () => {
+    num += "3";
+    display.textContent = num;
+});
+let four = document.querySelector("#four");
+four.addEventListener("click", () => {
+    num += "4";
+    display.textContent = num;
+});
+let five = document.querySelector("#five");
+five.addEventListener("click", () => {
+    num += "5";
+    display.textContent = num;
+});
+let six = document.querySelector("#six");
+six.addEventListener("click", () => {
+    num += "6";
+    display.textContent = num;
+});
+let seven = document.querySelector("#seven");
+seven.addEventListener("click", () => {
+    num += "7";
+    display.textContent = num;
+});
+let eight = document.querySelector("#eight");
+eight.addEventListener("click", () => {
+    num += "8";
+    display.textContent = num;
+});
+let nine = document.querySelector("#nine");
+nine.addEventListener("click", () => {
+    num += "9";
+    display.textContent = num;
+});
+let zero = document.querySelector("#zero");
+zero.addEventListener("click", () => {
+    num += "0";
+    display.textContent = num;
 });
 
-let two = document.querySelector("#two");
-let three = document.querySelector("#three");
-let four = document.querySelector("#four");
-let five = document.querySelector("#five");
-let six = document.querySelector("#six");
-let seven = document.querySelector("#seven");
-let eight = document.querySelector("#eight");
-let nine = document.querySelector("#nine");
-let zero = document.querySelector("#zero");
-
 let plus = document.querySelector("#plus");
+plus.addEventListener("click", () => {
+    operator = "+";
+    numContainer = num;
+    num = "";
+    display.textContent = operator;
+});
 let subtract = document.querySelector("#subtract");
+subtract.addEventListener("click", () => {
+    operator = "-";
+    numContainer = num;
+    num = "";
+    display.textContent = operator;
+});
 let multiply = document.querySelector("#multiply");
+multiply.addEventListener("click", () => {
+    operator = "x";
+    numContainer = num;
+    num = "";
+    display.textContent = operator;
+});
 let divide = document.querySelector("#divide");
+divide.addEventListener("click", () => {
+    operator = "/";
+    numContainer = num;
+    num = "";
+    display.textContent = operator;
+});
+
+
 
 let equals = document.querySelector("#equalsButton");
 equals.addEventListener("click", () => {
-    operate(operator, numOne, numTwo);
+    num = operate(operator, parseInt(numContainer), parseInt(num));
+    display.textContent = num;
+    operator = "";
+    numContainer = num;
+    num = "";
+    if (num == "ERROR") {
+        num = "";
+    }
 });
+
+let clear = document.querySelector("#clearButton");
+clear.addEventListener("click", () => {
+    num = "";
+    numContainer = "";
+    operator = "";
+    display.textContent = num;
+});
+
 
 function add(a, b) {
     return a + b;
@@ -44,21 +122,33 @@ function times(a, b) {
 function division(a, b) {
     return a / b;
 }
-function operate(operator, a, b) {
-    switch(operator) {
+function operate(oper, a, b) {
+    switch(oper) {
         case "+":
-            add(a, b);
-            break;
+            return add(a, b);
         case "-":
-            minus(a, b);
-            break;
+            return minus(a, b);
         case "x":
-            times(a, b);
-            break;
+            return times(a, b);
         case "/":
-            division(a, b);
-            break;
+            if (b == 0) {
+                console.log("Divide by 0 error");
+                display.textcontent = "ERROR";
+                return "ERROR";
+            }
+            return division(a, b);
+        case "":
+            console.log("No operands provided");
+            num = "";
+            numContainer = "";
+            operator = "";
+            display.textContent = num;
+            return "";
         default:
             console.log("No operands provided");
+            num = "";
+            numContainer = "";
+            operator = "";
+            display.textContent = num;
     }
 }
